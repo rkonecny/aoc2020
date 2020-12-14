e10927f2475ad5d3ea9fc2a5aa66f472c20c47f2
Roman Konecny <roman.konecny@konicaminolta.cz>
2020-12-14 17:52:27 +0100

    day 13 (ugh)

---
+++ 13/13_1.py
@@ -0,0 +1,17 @@
+with open("input", "r") as inputFile:
+    input = inputFile.readlines()
+
+departTimestamp = int(input[0])
+busLines = [int(x) for x in input[1].split(",") if x.isdigit()]
+
+earliestDepartures = []
+
+for i in busLines:
+    x = departTimestamp // i
+    earliestDepart = i * (x+1)
+    earliestDepartures.append(earliestDepart)
+
+firstDepart = min(earliestDepartures)
+index = earliestDepartures.index(firstDepart)
+busLine = busLines[index]
+print(busLine * (firstDepart - departTimestamp))
---
+++ 13/13_2.py
@@ -0,0 +1,12 @@
+import math
+with open("input", "r") as inputFile:
+    input = inputFile.readlines()
+
+busLines = [(int(x), int(x)-i)
+            for i, x in enumerate(input[1].split(",")) if x.isdigit()]
+
+
+# chinese remainder
+product = math.prod([x[0] for x in busLines])
+total = sum(y * pow(product // x, -1, x) * (product // x) for x, y in busLines)
+print(total % product)
